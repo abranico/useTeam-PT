ede53c00e4712794f8098aedd2d37f79467d2f0b
abranico <abranico011@gmail.com>
2025-10-07 18:35:18 -0300

    feat(backend): add Swagger documentation for the API

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { config } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.setGlobalPrefix('api');
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Swagger
+  const options = new DocumentBuilder()
+    .setTitle('Kanban-XP API')
+    .setDescription('API documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('/', app, document);
+
+  await app.listen(config.port, () => {
+    console.log('✅ Server listening on port', config.port);
+  });
 }
 bootstrap();
